197cc538d21657dc0a76814eb5ec9058db8532b3
Suzuki Ertiga Batam <suzukiertigabatam@gmail.com>
2018-06-11 05:34:02 +0700

    Update page.js

--- page.js
+++ page.js
@@ -82,3 +82,25 @@ $(document).ready(function(){
     });
   });
 })
+
+$(function(){
+    $('body').on('click', '.navbar-toggler', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('open');
+        $('.navbar-menu').toggleClass('open');
+        $('.navbar-menu').slideToggle();
+    });
+
+    $( document ).ajaxStart(function() {
+        $( "#loading" ).show();
+    });
+    $( document ).ajaxStop(function() {
+        $( "#loading" ).hide();
+    });
+});
+
+function openProduct (type = 'automobile') {
+    $('#productMenu').html('');
+    $('#productMenu').fadeIn();
+    $('#productMenu').load(base_url+'product/menu/'+type);
+}
